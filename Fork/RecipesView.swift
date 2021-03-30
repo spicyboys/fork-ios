7bee5d38b294ba8d3b065c6d8007d05826d8af40
Jacob <dev.jacob.w@gmail.com>
2021-03-29 20:35:21 -0500

    Added Tabviews for Recipes/Settings

---
+++ Fork/RecipesView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipesView.swift
+//  Fork
+//
+//  Created by Jacob Williamson on 3/29/21.
+//
+
+import SwiftUI
+
+struct RecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipesView()
+    }
+}
