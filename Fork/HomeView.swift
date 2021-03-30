6e94f436a3c74b400c2e44f5150ad681c3215979
Jacob <dev.jacob.w@gmail.com>
2021-03-29 20:47:43 -0500

    Properly separated Tabviews into their own view files

--- Fork/HomeView.swift
+++ Fork/HomeView.swift
@@ -11,7 +11,21 @@ import Firebase
 struct HomeView: View {
     let user: User
     
+    @State private var selection = 0
+    
     var body: some View {
-        Text(user.displayName!)
+        TabView(selection: $selection){
+            Text("Hi \(user.displayName!), this is the Recipes UI")
+                .tabItem {
+                    Text("Recipes")
+                }.tag(0)
+            Text("Hi \(user.displayName!), this is the Settings UI")
+                .font(.title3)
+                .tabItem {
+                    Text("Settings")
+                }.tag(1)
+        }
+        .font(.largeTitle)
     }
+
 }
---
+++ Fork/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Fork
+//
+//  Created by Jacob Williamson on 3/29/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
